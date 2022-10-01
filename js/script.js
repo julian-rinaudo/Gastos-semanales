cd1f86f23d11441f8f880e625ad6c0e9e06ec61a
julian-rinaudo <julianrinaudo18@gmail.com>
2022-10-01 01:07:54 -0300

    Proyecto nuevo

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//variables
+const divBudge = document.querySelector("#presupuesto");
+const askBudge = document.querySelector("#preguntarPresupuesto");
+const formularioPregunta = document.querySelector("#formulario-pregunta");
+//funciones
+const preguntarPresupuesto = e => {
+    e.preventDefault();
+    const presupuesto = askBudge.children[1].children[0].value;
+    console.log(presupuesto);
+
+    askBudge.classList.add("d-none");
+    divBudge.classList.remove("d-none")
+    divBudge.classList.add("d-flex");
+}    
+//clases
+class UI{
+    
+}
+
+const ui = new UI();
+
+//eventos
+formularioPregunta.addEventListener("submit", preguntarPresupuesto);
