91590f546bd569a2af93c2f5fb89c3eab8dca6dc
julian-rinaudo <julianrinaudo18@gmail.com>
2022-10-01 12:19:37 -0300

    s

--- js/script.js
+++ js/script.js
@@ -7,8 +7,24 @@ const formularioPregunta = document.querySelector("#formulario-pregunta");
 
 const preguntarPresupuesto = e => {
     e.preventDefault();
-    const presupuesto = askBudge.children[1].children[0].value;
-    console.log(presupuesto);
+    const input = askBudge.children[1].children[0];
+    const presupuesto = parseInt(input.value); 
+    console.log(presupuesto)
+    while (presupuesto === "" || isNaN(presupuesto) || presupuesto < 0) {
+        
+        const errorMessage = document.createElement("p");
+        errorMessage.classList.add("alert", "alert-danger");
+        input.classList.add("border-danger")
+        errorMessage.textContent = "Presupuesto No Valido";
+        setTimeout(() => {
+            errorMessage.remove()
+        }, 2000);
+
+        askBudge.insertBefore(errorMessage, formularioPregunta);
+        formularioPregunta.reset();
+        
+        return;
+    }
 
     askBudge.classList.add("d-none");
     divBudge.classList.remove("d-none")
